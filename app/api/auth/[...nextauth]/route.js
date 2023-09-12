22450f0796b5365d177dd7e049c1efb4747e3ea0
Ruslan Telitsyn <zimpuytel@gmail.com>
2023-09-12 20:14:47 +0300

    create prompt page and new prompt endpoint for db

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -11,35 +11,37 @@ const handler = NextAuth({
             clientSecret: process.env.GOOGLE_CLIENT_SECRET
         })
     ],
-    async session({ session }) {
-        const sessionUser = await User.findOne({ email: session.user.email });
+    callbacks: {
+        async session({ session }) {
+            const sessionUser = await User.findOne({ email: session.user.email });
 
-        session.user.id = sessionUser._id.toString();
+            session.user.id = sessionUser._id.toString();
 
-        return session;
-    },
-    async signIn({ profile }) {
-        try {
-            await connectToDB();
+            return session;
+        },
+        async signIn({ profile }) {
+            try {
+                await connectToDB();
 
-            // check if user exists
-            const userExists = await User.findOne({ email: profile.email });
+                // check if user exists
+                const userExists = await User.findOne({ email: profile.email });
 
-            // if not, create a new user
-            if (!userExists) {
-                await User.create({
-                    email: profile.email,
-                    username: profile.name.replace(" ", "").toLowerCase(),
-                    image: profile.picture
-                });
+                // if not, create a new user
+                if (!userExists) {
+                    await User.create({
+                        email: profile.email,
+                        username: profile.name.replace(" ", "").toLowerCase(),
+                        image: profile.picture
+                    });
+                }
+
+                return true;
+            } catch (error) {
+                console.error(error);
+                return false;
             }
-
-            return true;
-        } catch (error) {
-            console.error(error);
-            return false;
         }
-    }
+    },
 });
 
 export { handler as GET, handler as POST }
