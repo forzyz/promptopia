2c6d74cd040906fd0a4e76f9e41b7917048dea57
Ruslan Telitsyn <zimpuytel@gmail.com>
2023-09-17 18:32:16 +0300

    again fix path

--- app/profile/[id]/page.jsx
+++ app/profile/[id]/page.jsx
@@ -3,7 +3,7 @@
 import { useState, useEffect } from "react";
 import { useSearchParams } from "next/navigation";
 
-import Profile from "@components/profile";
+import Profile from "@components/Profile";
 
 const UserProfile = ({ params }) => {
   const searchParams = useSearchParams();
